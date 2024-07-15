3b68adffd54840bfb0a15c5c73269df80c31fb55
Kaua Fidelis <kauafidelis7@gmail.com>
2024-07-15 08:13:28 -0300

    Criei a rota de Post

--- 2_livraria/server.js
+++ 2_livraria/server.js
@@ -1,41 +1,120 @@
-import "dotenv/config"
-import  express, { application }  from "express"
-import mysql from "mysql2"
-import {v4 as uuidv4} from "uuid"
 
-const PORT = process.env.PORT
+
+
+/////////////////////
+
+import "dotenv/config" ;
+import express,{application, response} from "express";
+import mysql from "mysql2";
+import { v4 as uuidv4 } from "uuid";
+
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 
-// Criar conexão com o bando de dados MYSQL
-
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Sen@iDev77!.",
-    database: "livraria",
+    host: 'localhost',
+    user: 'root',
+    password:'Sen@iDev77!.',
+    database:'livraria', 
     port: 3306
-});
-
-// conectar ao banco de dados
+})
 
 conn.connect((err)=>{
     if(err){
-    return console.error(err.stack)
-    }
-    console.log("Mysql Connection")
-        app.listen(PORT, ()=>{
-            console.log("Servidor on PORT "+ PORT)
+        return console.error(err.stack);
+    };
+    
+    console.log("MySql Conectado");
+
+    app.listen(PORT, ()=>{
+        console.log('servidor na port', PORT);
     });
 });
 
-app.get("/", (request, response)=>{
-    response.send("Olá, Mundo!")
+
+app.get("/livros", (req, res)=>{
+    //query para banco de dados(consulta)
+    const sql = /*sql*/`SELECT * FROM livros`;
+
+    conn.query(sql, (err, data)=>{
+        if(err){
+            res.status(500).json({message:'Erro ao buscar livros'});
+            return console.log(err);
+        };
+
+        const livros = data;
+        res.status(200).json(livros);
+    });
 });
 
-//Rota 404
+app.post('/livros', (req, res)=>{
+    const {titulo, autor, ano_publicacao, genero, preco} = req.body;
 
-app.use((req,res)=>{
-    res.status(404).json({message: "Rota não encontrada."})
+    //validacoes
+    if(!titulo){
+        res.status(400).json({message: 'Campo obrigatório'});
+        return 
+    } else if(!autor){
+        res.status(400).json({message: 'Campo obrigatório'});
+        return 
+    } else if(!ano_publicacao){
+        res.status(400).json({message: 'Campo obrigatório'});
+        return 
+    } else if(!genero){
+        res.status(400).json({message: 'Campo obrigatório'});
+        return 
+    } else if(!preco){
+        res.status(400).json({message: 'Campo obrigatório'});
+        return 
+    };
+
+//Cadastrar um livro -> antes preciso saber se este livro existe
+    const checksql = /*sql*/ `
+    SELECT * FROM livros 
+    WHERE titulo = "${titulo}" AND 
+    autor = "${autor}" AND 
+    ano_publicacao = "${ano_publicacao}"`;
+
+    conn.query(checksql, (err, data)=>{
+        if(err){
+            res.status(500).json({message:'Erro ao buscar livros'});
+            return console.log(err);
+        };
+
+        if(data.length > 0){
+            res.status(409).json({message:"Esse livro já existe na livraria"});
+            return console.log(err);
+        }
+
+        const id = uuidv4()
+        const disponibilidade = 1
+
+        const insertSql = /*sql*/`INSERT INTO livros(id, titulo, autor, ano_publicacao, genero, preco, disponibilidade)
+        VALUES("${id}", "${titulo}", "${autor}", "${ano_publicacao}", "${genero}","${preco}","${disponibilidade}")`
+
+        conn.query(insertSql, (err) =>{
+            if(err){
+                res.status(500).json({message:'Erro ao cadastar livro'});
+                return console.log(err);
+            };
+
+            res.status(201).json({message:'Livro cadastrado'})
+        })
+
+    });
+});
+
+app.get('/livros/:id', (req, res)=>{
+    const {id} = request.params
 })
+app.put('/livros/:id', (req, res)=>{
+    const {id} = request.params
+})
+app.delete('/livros/:id', (req, res)=>{
+    const {id} = request.params
+})
+app.use((req, res)=>{
+    res.status(404).json({message:"Não encontrado"});
+});
